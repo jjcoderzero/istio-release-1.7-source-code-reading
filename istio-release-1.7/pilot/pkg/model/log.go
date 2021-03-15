2dc23de7cda7c205a55fd4f8440cff103cd6cd75
Andrew <Andrew.X.Wan.-ND@disney.com>
2021-03-15 23:35:41 +0800

    init

--- istio-release-1.7/pilot/pkg/model/log.go
+++
@@ -1,7 +0,0 @@
-package model
-
-import (
-	istiolog "istio.io/pkg/log"
-)
-
-var log = istiolog.RegisterScope("model", "model", 0)
--- istio-release-1.7/tools/istio-iptables/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"istio.io/istio/tools/istio-iptables/pkg/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
