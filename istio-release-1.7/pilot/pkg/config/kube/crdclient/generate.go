2372ce76fb9f36962528f243b3a3297f5cfc2316
Andrew <Andrew.X.Wan.-ND@disney.com>
2021-03-15 23:23:28 +0800

    init

--- istio-release-1.7/pilot/pkg/config/kube/crdclient/generate.go
+++
@@ -1,4 +0,0 @@
-package crdclient
-
-// nolint: lll
-//go:generate go run $REPO_ROOT/pilot/pkg/config/kube/crdclient/gen/main.go --template $REPO_ROOT/pilot/pkg/config/kube/crdclient/gen/types.go.tmpl --output $REPO_ROOT/pilot/pkg/config/kube/crdclient/types.gen.go
--- istio-release-1.7/tools/istio-clean-iptables/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"istio.io/istio/tools/istio-clean-iptables/pkg/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
